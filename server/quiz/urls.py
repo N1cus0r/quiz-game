d14ccb08988f634bb1488896ad461bde016fe242
N1cus0r <lino479Nick@mail.ru>
2023-04-14 19:15:16 +0300

    Formated code

--- server/quiz/urls.py
+++ server/quiz/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = "quiz"
 
 urlpatterns = [
-    path("get-game", views.GetGameState.as_view(), name='get-game'),
-    path("create-game", views.CreateGame.as_view(), name='create-game'),
-    path("delete-game", views.DeleteGame.as_view(), name='delete-game'),
+    path("get-game", views.GetGameState.as_view(), name="get-game"),
+    path("create-game", views.CreateGame.as_view(), name="create-game"),
+    path("delete-game", views.DeleteGame.as_view(), name="delete-game"),
 ]
--- server/rooms/urls.py
+++ server/rooms/urls.py
@@ -5,9 +5,9 @@ from . import views
 app_name = "rooms"
 
 urlpatterns = [
-    path("", views.GetRoomData.as_view(), name='get-room-data'),
-    path("create-room", views.CreateRoom.as_view(), name='create-room'),
-    path("join-room", views.JoinRoom.as_view(), name='join-room'),
-    path("leave-room", views.LeaveRoom.as_view(), name='leave-room'),
-    path("check-if-in-room", views.CheckIfInRoom.as_view(), name='check-if-in-room'),
+    path("", views.GetRoomData.as_view(), name="get-room-data"),
+    path("create-room", views.CreateRoom.as_view(), name="create-room"),
+    path("join-room", views.JoinRoom.as_view(), name="join-room"),
+    path("leave-room", views.LeaveRoom.as_view(), name="leave-room"),
+    path("check-if-in-room", views.CheckIfInRoom.as_view(), name="check-if-in-room"),
 ]
