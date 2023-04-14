31034e99ac8983fb38c4a5d7777c2b70f3ad8cf8
N1cus0r <lino479Nick@mail.ru>
2023-04-14 19:03:42 +0300

    Added reverse names for url paths

--- server/quiz/urls.py
+++ server/quiz/urls.py
@@ -6,5 +6,5 @@ app_name = "quiz"
 urlpatterns = [
     path("get-game", views.GetGameState.as_view(), name='get-game'),
     path("create-game", views.CreateGame.as_view(), name='create-game'),
-    path("delete-game", views.DeleteGame.as_view(), 'delete-game'),
+    path("delete-game", views.DeleteGame.as_view(), name='delete-game'),
 ]
